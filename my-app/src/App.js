82a6542637fcaa01a156d38a50cc5cdfbed2eb17
rohuna <agrawal.rohun@gmail.com>
2021-02-21 09:06:24 -0800

    backend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,12 +4,22 @@ import './App.css';
 import './import.css';
 import Navbar from './Navbar'
 
+import {initAdmin} from './backend/database.js';
+
 function App() {
+
+  testBackend();
+  
   return (
     <div className="App">
       <Navbar />
     </div>
   );
+}
+
+function testBackend()
+{
+  console.log(initAdmin())
 }
 
 export default App;
---
+++ my-app/src/backend/database.js
@@ -0,0 +1,24 @@
+// var admin = require('firebase-admin');
+
+// var serviceAccount = require("../facematch-6e6ff-firebase-adminsdk-cc3yf-160548092f.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: 'https://facematch-6e6ff.firebaseio.com'
+// });
+
+// const db = admin.firestore();
+
+// export async function initAdmin()
+// {
+  
+  
+//   const docRef = db.collection('users').doc('alovelace');
+  
+//   await docRef.set({
+//     first: 'Ada',
+//     last: 'Lovelace',
+//     born: 1815
+//   });
+  
+// }
